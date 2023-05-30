a43c195c6a5374f2a5f9a20414344645b280a403
Mestiri77 <faroukmestiri77@gmail.com>
2023-05-30 08:55:35 +0100

    fractionConverter

---
+++ fractionConverter.js
@@ -0,0 +1,22 @@
+/**
+                                                ▕╮╭┻┻╮╭┻┻╮╭▕╮╲
+                                                ▕╯┃╭╮┃┃╭╮┃╰▕╯╭▏
+                                                ▕╭┻┻┻┛┗┻┻┛ ╰▏
+                                                ▕╰━━━┓┈┈┈╭╮▕╭╮▏
+                                                ▕╭╮╰┳┳┳┳╯╰╯▕╰╯▏
+                                                ▕╰╯┈┗┛┗┛┈╭╮▕╮┈▏
+ *?            Mooooooooooooorning Jalysset , so our warmup today is one of the toy probelms we already did in the junior phase at RBK
+ *?                             Write a function that takes a number as its argument and
+ *?                         returns a string that represents that number's simplified fraction.
+ *
+ *?                                       Example: toFraction(0.5) === '1/2'
+ *
+ **                       Whole numbers and mixed fractions should be returned as irregular fractions
+ *
+ **                                  Example: toFraction(3.0) === '3/1'
+ *
+ **                                  Example: toFraction(2.5) === '5/2'
+ *
+ * 
+ *!                                           Happy coding have fun  
+ */
