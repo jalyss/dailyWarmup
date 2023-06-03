b5e3b3f02ae3b2a1eb7275e637c09c36838c7c2a
Mestiri77 <faroukmestiri77@gmail.com>
2023-06-03 03:37:38 +0100

    Stairs

---
+++ Stairs.js
@@ -0,0 +1,23 @@
+    /**
+     **               Good Morning everyone , it's WU (warm up ) time ¯\_( ͡ᵔ ͜ʖ ͡ᵔ )_/¯
+     ** (ps : The purpose of this is to build your muscle memory and increase your problem solving skills )
+     ** 
+**    Imagine you are running up a staircase with n steps and can hop either 1 step, 2 steps at a time.
+**   Implement a method to count how many possible ways you can run up the stairs if given the number of steps in the staircase as N.
+** 
+**  
+**   
+*!1.) 
+*!Input: 3
+*!Output: 4
+*! 
+*!2.) 
+*!Input: 4
+*!Output: 7
+*! 
+*!3.)
+*!Input: 50
+*!Output: 10562230626642
+**
+*? HAPPY coding  ≧◠‿◠≦✌
+*/
